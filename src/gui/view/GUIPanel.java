c510704ba37d574db4ad44c7ead6ad0090da49b2
Jac0bAnderson <jake912235@gmail.com>
2015-11-09 09:54:39 -0700

    gui
    crappy GUI example

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,8 +3,11 @@ package gui.view;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;//for layout
 // import javax.swing.*; imports the whole javax lib
+
 import gui.controller.controller;
+import javax.swing.SwingConstants;// controller.java import
 
 /**
  * 
@@ -16,25 +19,31 @@ public class GUIPanel extends JPanel
    private controller baseController;
    private JButton firstButton;//you need to import "javax.swing.JButton
    private JTextField firstTextField;//you need to import javax.swing.JTextField
+   private SpringLayout baseLayout;
 
 public GUIPanel(controller baseController)  // baseController is a parameter; you have to use your parameters
 {
 	this.baseController = baseController;   // baseController is used.
 	
 	firstButton = new JButton("dont click plz");
+	firstButton.setVerticalAlignment(SwingConstants.TOP);
 	firstTextField = new JTextField("type stuff b0ss");
 	
 	setupPanel();       //sets up panel
 	setupLayout();     //         Layout
 	setupListeners(); //      and Listeners
 }
-
+/**
+ * helper method to load all GUI components into the panel.
+ */
 private void setupPanel()
 {
-	
+	this.setLayout(baseLayout);
+	this.add(firstButton);
+	this.add(firstTextField);
 }
 
-private void setupLayout()
+private void setupLayout()//garbage for desgin tab code
 {
 	
 }
